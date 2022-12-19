c716245f76e0ef41597338cb2f7a625c13dca6e6
Larysa Cherneha <larisa345274578@gmail.com>
2022-12-19 00:55:42 +0000

    homework2

--- pages/register.js
+++ pages/register.js
@@ -1,35 +1,35 @@
 const { I } = inject();
 
 module.exports = {
-registerHeaderText: 'Register Account',
-firstNameField: { css: '#input-firstname' },
-lastNameField: { css: '#input-lastname' },
-email: { css: '#input-email' },
-telephone: { css: '#input-telephone' },
-password: {css: '#input-password'},
-confirmPassword: {css: '#input-confirm'},
-privacyBox: {xpath: '//*[@id="content"]/form/div/div/input[1]'},
-clickContinue: {xpath: '//*[@id="content"]/form/div/div/input[2]'},
-registerSuccessText: 'Your Account Has Been Created!',
-  
-verifyRegisterAccountText(){
-I.see(this.registerHeaderText);
-},
+    registerHeaderText: 'Register Account',
+    firstNameField: { css: '#input-firstname' },
+    lastNameField: { css: '#input-lastname' },
+    email: { css: '#input-email' },
+    telephone: { css: '#input-telephone' },
+    password: { css: '#input-password' },
+    confirmPassword: { css: '#input-confirm' },
+    privacyBox: { xpath: '//*[@id="content"]/form/div/div/input[1]' },
+    clickContinue: { xpath: '//*[@id="content"]/form/div/div/input[2]' },
+    registerSuccessText: 'Your Account Has Been Created!',
 
-submitRegistrationForm(user) {
-I.fillField(this.firstNameField, user.firstName);
-I.fillField(this.lastNameField, user.lastName);
-I.fillField(this.email, user.email);
-I.fillField(this.telephone, user.telephone);
-I.fillField(this.password, user.password);
-I.fillField(this.confirmPassword, user.confirmPassword);
-I.click(this.privacyBox);
-I.click(this.clickContinue);
-},
+    verifyRegisterAccountText() {
+        I.see(this.registerHeaderText);
+    },
 
-verifyRegisterSuccessText(){
-I.see(this.registerSuccessText);
-}
+    submitRegistrationForm(user) {
+        I.fillField(this.firstNameField, user.firstName);
+        I.fillField(this.lastNameField, user.lastName);
+        I.fillField(this.email, user.email);
+        I.fillField(this.telephone, user.telephone);
+        I.fillField(this.password, user.password);
+        I.fillField(this.confirmPassword, user.confirmPassword);
+        I.click(this.privacyBox);
+        I.click(this.clickContinue);
+    },
+
+    verifyRegisterSuccessText() {
+        I.see(this.registerSuccessText);
+    }
 
 };
 // insert your locators and methods here
--- steps_file.js
+++ steps_file.js
@@ -1,17 +1,59 @@
-// in this file you can append custom step methods to 'I' object
- const STORE_URL = 'http://opencart.qatestlab.net/index.php';
+
+const signInButton = { xpath: '//a[text()="Sign In"]' };
+const emailField = { css: '#input-email' };
+const passwordField = { css: '#input-password' };
+const submitButton = { xpath: '//input[@type="submit"]' };
+const addToCartButton = { xpath: '//*[@id="button-cart"]' };
+const iconCart = { xpath: '//*[@id="cart-total2"]' };
+const checkoutButton = { xpath: '//*[@id="cart"]/ul/li[3]/div/a[2]' };
+const radioButtonNewAddress = { xpath: '//*[@id="collapse-payment-address"]/div/form/div[3]/label' };
+const radioButtonNewShippingAddress = { css: '#collapse-shipping-address > div > form > div:nth-child(3) > label' };
 
 
-module.exports = function() {
+
+const STORE_URL = 'http://opencart.qatestlab.net/index.php';
+const PRODUCT_URL = 'http://opencart.qatestlab.net/index.php?route=product/product&path=31&product_id=40';
+const ORDER_HISTORY = 'http://opencart.qatestlab.net/index.php?route=account/order';
+
+module.exports = function () {
   return actor({
-openStore(){
-  this.amOnPage(STORE_URL);
-}
-    
+    openStore() {
+      this.amOnPage(STORE_URL);
+    },
+
+    openProductPage() {
+      this.amOnPage(PRODUCT_URL);
+    },
+
+    openOrderPage() {
+      this.amOnPage(ORDER_HISTORY);
+    },
+
+    login(user) {
+      this.openStore();
+      this.click(signInButton);
+      this.fillField(emailField, user.email);
+      this.fillField(passwordField, user.password);
+      this.click(submitButton);
+    },
+
+    cardProcess() {
+      this.click(addToCartButton);
+      this.click(iconCart);
+      this.click(checkoutButton);
+
+    },
+
+    changeAddress() {
+      this.click(radioButtonNewAddress);
+    },
+
+    changeShippingAddress() {
+      this.click(radioButtonNewShippingAddress);
+    },
+
 
   });
+
+
 }
-
-
-// Define custom steps here, use 'this' to access default methods of I.
-    // It is recommended to place a general 'login' function here.
